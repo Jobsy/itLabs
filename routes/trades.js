d1c37a2ac39ae215ed518ddec1ece03907e00e5c
Oluwajoba Bello <oluwajobabello@gmail.com>
2023-01-13 06:17:39 +0100

    commit solutions

--- routes/trades.js
+++ routes/trades.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const tradesController = require('../controllers/trades');
+
+router.post('/', tradesController.createTrade);
+router.get('/', tradesController.getAllTrades);
+router.get('/:id', tradesController.getTradeById);
+router.put('/:id', tradesController.putTradeById);
+router.patch('/:id', tradesController.patchTradeById);
+router.delete('/:id', tradesController.deleteTradeById);
+
 
 module.exports = router;
